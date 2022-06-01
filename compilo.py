1b0608eb24b2f4b3e1d09f106cf0d4ee7a6eb475
leo <leobertrand641@gmail.com>
2022-06-01 09:57:19 +0200

    t

---
+++ compilo.py
@@ -0,0 +1,163 @@
+import lark
+grammaire = lark.Lark("""
+variables : typed_variable (","  typed_variable)*
+expr :  IDENTIFIANT -> variable | NUMBER -> nombre | "'" STR "'"-> str | "malloc" "(" expr ")" -> malloc | IDENTIFIANT ".cAt" "(" expr ")" -> cat 
+| expr OP expr -> binexpr | "(" expr ")" -> parenexpr | POINTER expr -> valeur | "&" IDENTIFIANT -> adresse | "len(" expr ")" -> len
+cmd : IDENTIFIANT "=" expr ";"-> assignment | POINTER IDENTIFIANT "=" expr ";"-> assignment1 |"while" "(" expr ")" "{" bloc "}" -> while
+    | "if" "(" expr ")" "{" bloc "}" -> if | "printf" "(" expr ")" ";"-> printf | typed_variable ";" -> variable
+POINTER : /[*]+/
+bloc : (cmd)*
+prog : "main" "(" variables ")" "{" bloc "return" "(" expr ")" ";" "}"
+typed_variable : BASIC_TYPE  IDENTIFIANT
+BASIC_TYPE : /(int|str)[*]*/
+STR : /[a-zA-Z0-9]+/
+NUMBER : /[0-9]+/
+OP : /[+\*>-]/
+IDENTIFIANT : /[a-zA-Z][a-zA-Z0-9]*/
+%import common.WS
+%ignore WS
+""", start = "prog")
+cpt=iter(range(10000))
+op2asm={"+":"add rax,rbx","-":"sub rax,rbx"}
+
+def pp_variables(vars):
+    return ", ".join([f"{t.children[0]} {t.children[1]}" for t in vars.children])
+
+
+def pp_expr(expr):
+    print( "hey", expr)
+    if expr.data in {"variable", "nombre"}:
+        return expr.children[0].value
+    if expr.data =="str":
+        return f"'{expr.children[0].value}'"
+    elif expr.data == "valeur":
+        e1 = pp_expr(expr.children[0])
+        return f"*{e1}"
+    elif expr.data == "adresse":
+        return f"&{expr.children[0].value}"
+    elif expr.data == "malloc":
+        e1 = pp_expr(expr.children[0])
+        return f"malloc({e1})"
+    elif expr.data == "cat":
+        e1 = pp_expr(expr.children[1])
+        return f"{expr.children[0]}.cAt({e1})"
+    elif expr.data == "len":
+        e1 = pp_expr(expr.children[0])
+        return f"len({e1})"
+    elif expr.data == "binexpr":
+        e1 = pp_expr(expr.children[0])
+        e2 = pp_expr(expr.children[2])
+        op = expr.children[1].value
+        return f"{e1} {op} {e2}"
+    elif expr.data == "parenexpr":
+        return f"({pp_expr(expr.children[0])})"
+    else:
+        raise Exception("Not implemented")
+
+
+def pp_cmd(cmd):
+    if cmd.data=="assignment":
+        lhs = cmd.children[0]#cmd.children[0].value
+        rhs = pp_expr(cmd.children[1])
+        return f"{lhs} = {rhs};\n"
+    elif cmd.data=="assignment1":
+        lhs=cmd.children[0]+cmd.children[1]
+        rhs=pp_expr(cmd.children[2])
+        return f"{lhs} = {rhs};\n"
+    elif cmd.data == "printf":
+        return f"printf( {pp_expr(cmd.children[0])} );\n"
+    elif cmd.data in {"while", "if"}:
+        e = pp_expr(cmd.children[0])
+        b = pp_bloc(cmd.children[1])
+        return f"{cmd.data} ({e}) {{ {b}}}"
+    elif cmd.data == "variable":
+        return f"{cmd.children[0].children[0]} {cmd.children[0].children[1]};"
+    else:
+        raise Exception("Not implemented")
+
+
+def pp_bloc(bloc):
+    return "\n".join([pp_cmd(t) for t in bloc.children])
+
+
+def pp_prg(prog):
+    vars = pp_variables(prog.children[0])
+    bloc = pp_bloc(prog.children[1])
+    ret = pp_expr(prog.children[2])
+    return f"main ({vars}){{\n {bloc} return ({ret});\n}}"
+
+def var_list(ast):
+    if isinstance(ast, lark.Token):
+        if ast.type == "IDENTIFIANT":
+            return {ast.value}
+        else:
+            return set()
+    s = set()
+    for c in ast.children:
+        s.update(var_list(c))
+    return s
+
+def compile(prg):
+    with open("moule.asm") as f:
+        code=f.read()
+        var_decl="\n".join([f"{x} : dq 0" for x in var_list(prg)])
+        code=code.replace("VAR_DECL",var_decl)
+        code=code.replace("RETURN",compile_expr(prg.children[2]))
+        code=code.replace("BODY",compile_bloc(prg.children[1]))
+        code = code.replace("VAR_INIT",compile_vars(prg.children[0]))
+        return code
+
+def compile_expr(expr):
+    if expr.data == "variable":
+        return f"mov rax, [{expr.children[0].value}]"
+    elif expr.data == "nombre":
+        return f"mov rax, {expr.children[0].value}"
+    elif expr.data == "binexpr":
+        e1 = compile_expr(expr.children[0])
+        e2 = compile_expr(expr.children[2])
+        op = expr.children[1].value
+        return f"{e2}\npush rax\n{e1}\npop rbx\n{op2asm[op]}"
+
+
+    elif expr.data == "parenexpr":
+        return compile_expr(expr.children[0])
+    else:
+        raise Exception("Not implemented")
+
+def compile_vars(ast):
+    s=""
+    for i in range(len(ast.children)):
+        s +=f"mov rbx, [rbp-0x10]\nmov rdi,[rbx+{8*(i+1)}]\ncall atoi\nmov [{ast.children[i].value}], rax\n"
+    return s
+
+def compile_cmd(cmd):
+    if cmd.data == "assignment":
+        lhs = cmd.children[0].value
+        rhs = compile_expr(cmd.children[1])
+        return f"{rhs}\nmov [{lhs}],rax"
+    elif cmd.data == "printf":
+        return f"printf( {pp_expr(cmd.children[0])} );"
+    elif cmd.data == "while":
+        e = compile_expr(cmd.children[0])
+        b = compile_bloc(cmd.children[1])
+        index=next(cpt)
+        return f"debut{index}:{e}\ncmp rax,0\njz fin{index}\n{b}\njmp debut{index}\nfin{index}:\n"
+    else:
+        raise Exception("Not implemented")
+
+def compile_bloc(bloc):
+    return "\n".join([compile_cmd(t) for t in bloc.children])
+
+prg = grammaire.parse("""main(int X, int Z) {
+    p=malloc(10);
+    str test;
+    test='com'+'com';
+    str t;
+    t=test.cAt(2);
+    t=len(t);
+return(test); }""")
+print(prg)
+prg2 = pp_prg(prg)
+print(prg2)
+#print(compile(prg))
+
